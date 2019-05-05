4d4c7b3d58d3e35db68d0921fdd926b809517c5d
Szymon Murawski <szymon.murawski@gmail.com>
2019-05-05 18:09:08 +0200

    Game rules

---
+++ GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+/*
+ * 1. 2D grid
+ * 2. Cell is dead or al;ive
+ * 3. when we generate new sate
+ * 3a cell is alive neighbours = 0,1 = dead
+ * 3b cell is alive neighbours= 2,3 = alive
+ * 3c cell is alive neighboues >3 = dead
+ * 3d cell is dead neighbours ==3 = alive
+*/
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            bool[,] grid = new bool[10, 10];
+
+        }
+    }
+}
