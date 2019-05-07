71e92b14a9326e1f6e52606c70500040e1c94793
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-07 17:45:00 +0200

    Extracted printing a grid to function

--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -16,27 +16,17 @@ namespace GameOfLife
 
         {
             bool[,] grid = new bool[10, 10];//false - cell is dead, true - cell is alive
-            for (int y = 0; y <10; y++)
-            {
-                for (int x = 0; x < 10; x++)
-                {
-                    if (grid[x, y] == true)
-                    {
-                        Console.Write("L");         
-                    }
-                    else
-                    {
-                        Console.Write("D");
-                    }
-                }
-                Console.WriteLine();
-                
-            }
+            Print2DGrid(grid);
             grid[1, 1] = true;
             grid[1, 2] = true;
             grid[1, 3] = true;
-            grid[7,8 ] = true;
+            grid[7, 8] = true;
             grid[9, 9] = true;
+            Print2DGrid(grid);
+        }
+
+        private static void Print2DGrid(bool[,] grid)
+        {
             for (int y = 0; y < 10; y++)
             {
                 for (int x = 0; x < 10; x++)
