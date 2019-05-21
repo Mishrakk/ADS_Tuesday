56def94988097b704e31c136869d0220be57d0f7
Student <Student@DESKTOP-6QVR8SB>
2019-05-21 18:12:03 +0200

    Insertion sort

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -25,7 +25,17 @@ namespace Sorting
         }
         public static void InsertionSort(int[] array)
         {
-
+            for (int i = 1; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && array[j - 1] > array[j]) 
+                {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                    j--;
+                }
+            }
         }
     }
 }
