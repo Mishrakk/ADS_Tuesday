582f9d44e096c047ace68e7330e232a6cd27891b
Student <Student@DESKTOP-6QVR8SB>
2019-05-28 17:56:17 +0200

    First half of cocktailsort

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -42,10 +42,21 @@ namespace Sorting
             bool swapped = true;
             int start = 0;
             int end = array.Length;
-
-
-
-
+            while (swapped)
+            {
+                swapped = false;
+                for (int i = start; i < end -1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                end--;
+            }
         }
     }
 }
