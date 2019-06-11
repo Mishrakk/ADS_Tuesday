7385b22d68e47c87809a888653d207802f30d467
Student <Student@DESKTOP-6QVR8SB>
2019-06-11 18:02:26 +0200

    Beginning of stack

--- RPNCalculator/RPNCalculator/Program.cs
+++ RPNCalculator/RPNCalculator/Program.cs
@@ -8,4 +8,27 @@ namespace RPNCalculator
         {
         }
     }
+
+    public class Stack
+    {
+        int[] array = new int[10];
+        int count;
+        public Stack()
+        {
+            count = 0;
+        }
+
+        public void Push(int newElement)
+        {
+
+        }
+        public int Pop()
+        {
+
+        }
+        public int Peek()
+        {
+
+        }
+    }
 }
