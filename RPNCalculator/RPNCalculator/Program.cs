808b1e666618bd9a13e1ef561221a5b719439be8
Student <Student@DESKTOP-6QVR8SB>
2019-06-18 16:41:21 +0200

    We will use stack from c# defaults

--- RPNCalculator/RPNCalculator/Program.cs
+++ RPNCalculator/RPNCalculator/Program.cs
@@ -1,35 +1,12 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace RPNCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
-
-    public class Stack
-    {
-        int[] array = new int[10];
-        int count;
-        public Stack()
-        {
-            count = 0;
-        }
-
-        public void Push(int newElement)
-        {
-            array[count]= newElement;
-            count++;
-        }
-        public int Pop()
-        {
-
-        }
-        public int Peek()
-        {
-
+            
         }
     }
 }
